b641025c7b6bf2bef0105c469125e0dcbed62b73
samikshya22 <sharmasamikshya389@gmail.com>
2024-09-29 16:54:12 +0545

    update

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+  // define('db_server','localhost');
+  // define('db_username','root');
+  // define('db_password','');
+  // define('db_database','Library management system');
+  // $conn=mysqli_connect(db_server,db_username,db_password,db_database);
+  $conn=mysqli_connect("localhost","root","","Library management system");
+
+?>
---
+++ login.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+       <head>
+	        <meta charset="utf-8">
+	        <title>Welcome to homepage</title>
+			<link rel="stylesheet" href="style.css">
+	   </head>
+	   <body>
+	   <div class="loginbox">
+<h3>Login</h3>
+<form method="post" action="newfile.php">
+     <p>Username</p>
+     <input type="text" name="username">
+     <p>Password</p>
+     <input type="password" name="password">
+     <input type="submit" name="submit" value="Submit">
+				</form>
+			</div>
+	   </body>
+</html>
